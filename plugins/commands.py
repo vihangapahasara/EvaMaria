c01788e472405e6017cc024fc4263f35b1ab6612
jijilaay <89651549+jijilaay@users.noreply.github.com>
2021-11-06 12:47:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -35,7 +35,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ මගේ නිර්මාතෘ  ➕', url=f'http://t.me/viha_is_power')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
@@ -76,7 +76,7 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ මගේ නිර්මාතෘ➕', url=f'http://t.me/viha_is_power')
+            InlineKeyboardButton('➕ මගේ නිර්මාතෘ ➕', url=f'http://t.me/viha_is_power')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
