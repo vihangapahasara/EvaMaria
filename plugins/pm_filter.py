9d3f24b231e945864a5eb48b9e051682dd3e4df1
jijilaay <89651549+jijilaay@users.noreply.github.com>
2021-11-20 19:27:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -617,7 +617,15 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"🗓 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_{req}_{key}_{offset}")]
+
+            [InlineKeyboardButton(text=f"📰 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="ඊළඟ ➡⏩",callback_data=f"next_{req}_{key}_{offset}")
+
+],[
+
+InlineKeyboardButton('my father', url='https://t.me/viha_is_power'),
+
+InlineKeyboardButton('share our group' , url='https://t.me/share/url?url=https://t.me/joinchat/Q1uroGQ645U1OTg1&text=සුපිරි_Movie_Group_එකක්_තියනව._Film_එකේ_නම_දැම්ම_ගමන්_Film_එක_දෙන්නව._ඔන්න_Link_එක._ඉක්මනට_Join_වෙන්න'),]
+
         )
     else:
         btn.append(
