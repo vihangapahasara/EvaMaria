7d8da5336c639f26a9fc9e108160d7d16c21e2f0
jijilaay <89651549+jijilaay@users.noreply.github.com>
2021-11-21 13:34:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -124,12 +124,7 @@ async def next_page(bot, query):
         btn.append(
             [InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"), InlineKeyboardButton(f"📃 Pages {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages")]
         )
-         btn.append(
-
-            [InlineKeyboardButton(text=f"🤖My Father",url="https://t.me/viha_is_power"), InlineKeyboardButton(text="🔗Share Our Group",url=f"https://t.me/joinchat/Q1uroGQ645U1OTg1&text=සුපිරි%20Movie%20Group%20එකක්_තියනව.%20Film%20එකේ%20නම%20දැම්ම%20ගමන්%20Film%20එක%20දෙන්නව.%20ඔන්න%20Link%20එක.%20ඉක්මනට%20Join%20වෙන්න")]
-
-        )
-         
+        
     elif off_set is None:
         btn.append([InlineKeyboardButton(f"💌 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"), InlineKeyboardButton("ඊළඟ පිටුව�👉 ⏩", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
@@ -139,6 +134,11 @@ async def next_page(bot, query):
                 InlineKeyboardButton(f"💌 {round(int(offset)/10)+1} / {round(total/10)}", callback_data="pages"),
                 InlineKeyboardButton("ඊළඟ පිටුවට ⏩", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
+        )       
+         btn.append(
+
+            [InlineKeyboardButton(text=f"My Father",url="https://t.me/viha_is_power"), InlineKeyboardButton(text="Share Our Group",url=f"https://t.me/joinchat/Q1uroGQ645U1OTg1&text=සුපිරි%20Movie%20Group%20එකක්_තියනව.%20Film%20එකේ%20නම%20දැම්ම%20ගමන්%20Film%20එක%20දෙන්නව.%20ඔන්න%20Link%20එක.%20ඉක්මනට%20Join%20වෙන්න")]
+
         )
     try:
         await query.edit_message_reply_markup( 
