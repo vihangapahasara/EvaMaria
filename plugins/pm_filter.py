1537ec1a733f49f0554d98df2d83a4730e661f35
jijilaay <89651549+jijilaay@users.noreply.github.com>
2021-11-21 13:37:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,11 +135,7 @@ async def next_page(bot, query):
                 InlineKeyboardButton("ඊළඟ පිටුවට ⏩", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )       
-         btn.append(
-
-            [InlineKeyboardButton(text=f"My Father",url="https://t.me/viha_is_power"), InlineKeyboardButton(text="Share Our Group",url=f"https://t.me/joinchat/Q1uroGQ645U1OTg1&text=සුපිරි%20Movie%20Group%20එකක්_තියනව.%20Film%20එකේ%20නම%20දැම්ම%20ගමන්%20Film%20එක%20දෙන්නව.%20ඔන්න%20Link%20එක.%20ඉක්මනට%20Join%20වෙන්න")]
-
-        )
+         
     try:
         await query.edit_message_reply_markup( 
             reply_markup=InlineKeyboardMarkup(btn)
